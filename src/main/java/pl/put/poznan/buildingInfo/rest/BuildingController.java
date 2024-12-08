ab938ce5744ff5d0c901a376387bd5ba560043c2
Fiflon <odzera.dotrofera@gmail.com>
2024-12-08 21:29:19 +0100

    ADD LevelController

--- src/main/java/pl/put/poznan/buildingInfo/rest/BuildingController.java
+++ src/main/java/pl/put/poznan/buildingInfo/rest/BuildingController.java
@@ -1,13 +1,19 @@
 package pl.put.poznan.buildingInfo.rest;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
-import pl.put.poznan.buildingInfo.logic.Building;
 
-import java.util.ArrayList;
-import java.util.List;
+import pl.put.poznan.buildingInfo.logic.Building;
 
 
 //@RestController
@@ -52,7 +58,7 @@ public class BuildingController {
 
     private static final Logger logger = LoggerFactory.getLogger(BuildingController.class);
 
-    private List<Building> buildings = new ArrayList<Building>();
+    public List<Building> buildings = new ArrayList<Building>();
 
     @GetMapping("/all-buildings")
     public List<Building> getAllBuildings() {
